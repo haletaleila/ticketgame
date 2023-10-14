4d7cce1e01d73d3430c70bbc6344bae4380a0880
haletaleila <leilahaleta@gmail.com>
2023-10-14 13:15:24 +0900

    collii

--- src/ticketInter/concert/component/ConcertArea.jsx
+++ src/ticketInter/concert/component/ConcertArea.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const ConcertAreaArea = styled.area`
+  cursor: pointer;
+`;
 
 function ConcertArea({ coords, code }) {
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
-    <area shape="rect" coords={coords} />
+    <ConcertAreaArea shape="rect" coords={coords}></ConcertAreaArea>
   );
 }
 
